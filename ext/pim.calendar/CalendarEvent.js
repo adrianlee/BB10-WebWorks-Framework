4c8b799f2dc427da6eb49bcd6ad52d4b77aebc5e
Rosa Tse <rtse@rim.com>
2012-09-26 15:16:23 -0400

    New branch for pim.calendar

---
+++ ext/pim.calendar/CalendarEvent.js
@@ -0,0 +1,240 @@
+/*
+ * Copyright 2012 Research In Motion Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var CalendarEvent,
+    _ID = require("./manifest.json").namespace, // normally 2nd-level require does not work in client side, but manifest has already been required in client.js, so this is ok
+    utils = require("./../../lib/utils"),
+    CalendarError = require("./CalendarError"),
+    CalendarRepeatRule = require("./CalendarRepeatRule"),
+    CalendarFolder = require("./CalendarFolder");
+
+/**
+ * Contains information about a single calendar event.
+ * @constructor
+ * @param properties
+ */
+CalendarEvent = function (properties) {
+    var privateId,
+        privateParentId,
+        privateFolder;
+
+    this.allDay = properties && properties.allDay !== undefined ? properties.allDay : null;
+    this.attendees = properties && properties.attendees !== undefined ? properties.attendees : [];
+    this.birthday = properties && properties.birthday !== undefined ? properties.birthday : null;
+    this.description = properties && properties.description !== undefined ? properties.description : "";
+    this.end = properties && properties.end !== undefined ? (properties.end instanceof Date ? properties.end : new Date(parseInt(properties.end, 10))) : null;
+    this.location = properties && properties.location !== undefined ? properties.location : "";
+    this.reminder = properties && properties.reminder !== undefined ? properties.reminder : "";
+    this.recurrence = properties && properties.recurrence !== undefined ? properties.recurrence : null;
+    this.sensitivity = properties && properties.sensitivity !== undefined ? properties.sensitivity : null;
+    this.status = properties && properties.status !== undefined ? properties.status : "";
+    this.start = properties && properties.start !== undefined ? (properties.start instanceof Date ? properties.start : new Date(parseInt(properties.start, 10))) : null;
+    this.summary = properties && properties.summary !== undefined ? properties.summary : "";
+    this.timezone = properties && properties.timezone !== undefined ? properties.timezone : "";
+    this.transparency = properties && properties.transparency !== undefined ? properties.transparency : 2; // default to busy if not set
+    this.originalStartTime = properties && properties.originalStartTime !== undefined ? (properties.originalStartTime instanceof Date ? properties.originalStartTime : new Date(parseInt(properties.originalStartTime, 10))) : null;
+
+    privateId = properties && properties.id !== undefined ? properties.id : null;
+    privateParentId = properties && properties.parentId !== undefined ? properties.parentId : null;
+    privateFolder = properties && properties.folder !== undefined ? new CalendarFolder(properties.folder) : null;
+
+    Object.defineProperty(this, "id", { "value": privateId });
+    Object.defineProperty(this, "parentId", { "value": privateParentId });
+    Object.defineProperty(this, "folder", {"value": privateFolder });
+};
+
+CalendarEvent.prototype.save = function (onSaveSuccess, onSaveError) {
+    var args = {},
+        key,
+        innerKey,
+        successCallback = onSaveSuccess,
+        errorCallback = onSaveError,
+        saveCallback,
+        errorObj;
+
+    for (key in this) {
+        if (this.hasOwnProperty(key) && this[key] !== null) {
+            if (Object.prototype.toString.call(this[key]) === "[object Object]") {
+                args[key] = {};
+
+                for (innerKey in this[key]) {
+                    if (this[key][innerKey] !== null) {
+                        args[key][innerKey] = this[key][innerKey];
+                    }
+                }
+            } else if (Object.prototype.toString.call(this[key]) === "[object Date]") {
+                args[key] = this[key].toISOString();
+            } else {
+                args[key] = this[key];
+            }
+        }
+    }
+
+    if (args.attendees) {
+        if (this.folder) {
+            // if folder is defined for this event, check if folder supports participants
+            if (!this.folder.supportsParticipants) {
+                if (errorCallback) {
+                    errorObj = new CalendarError(CalendarError.INVALID_ARGUMENT_ERROR);
+                    errorCallback(errorObj);
+                }
+                return;
+            }
+        }
+
+        for (key in args.attendees) {
+            if (args.attendees[key].contactId && !window.isNaN(args.attendees[key].contactId)) {
+                args.attendees[key].contactId = window.parseInt(args.attendees[key].contactId);
+            }
+
+            if (args.attendees[key].id && !window.isNaN(args.attendees[key].id)) {
+                args.attendees[key].id = window.parseInt(args.attendees[key].id);
+            }
+        }
+    }
+
+    if (args.recurrence) {
+        if (args.recurrence.exceptionDates) {
+            for (key in args.recurrence.exceptionDates) {
+                args.recurrence.exceptionDates[key] = args.recurrence.exceptionDates[key].toISOString();
+            }
+        }
+
+        if (args.recurrence.expires) {
+            args.recurrence.expires = args.recurrence.expires.toISOString();
+        }
+    }
+
+    if (this.id && !window.isNaN(this.id)) {
+        args.id = window.parseInt(this.id);
+    }
+
+    if (this.folder && this.folder.accountId && !window.isNaN(this.folder.accountId)) {
+        args.accountId = window.parseInt(this.folder.accountId);
+    }
+
+    if (this.folder && this.folder.id && !window.isNaN(this.folder.id)) {
+        args.folderId = window.parseInt(this.folder.id);
+    }
+
+    if (this.parentId && !window.isNaN(this.parentId)) {
+        args.parentId = window.parseInt(this.parentId);
+    }
+
+    args._eventId = utils.guid();
+
+    console.log(args);
+
+    saveCallback = function (args) {
+        var result = JSON.parse(unescape(args.result)),
+            errorObj,
+            newEvent;
+
+        console.log("Save result!");
+        console.log(result);
+
+        if (result._success) {
+            if (successCallback) {
+                //contactUtils.populateContact(result);
+                if (result.event.recurrence) {
+                    result.event.recurrence = new CalendarRepeatRule(result.event.recurrence);
+                }
+
+                newEvent = new CalendarEvent(result.event);
+                successCallback(newEvent);
+            }
+        } else {
+            if (errorCallback) {
+                errorObj = new CalendarError(result.code);
+                errorCallback(errorObj);
+            }
+        }
+    };
+
+    window.webworks.event.once(_ID, args._eventId, saveCallback);
+    return window.webworks.execAsync(_ID, "save", args);
+};
+
+CalendarEvent.prototype.remove = function (onRemoveSuccess, onRemoveError) {
+    var args = {},
+        successCallback = onRemoveSuccess,
+        errorCallback = onRemoveError,
+        removeCallback;
+
+    args.accountId = window.parseInt(this.folder.accountId); // TODO folder is not set unless the developer re-save the event upon creation, need to check this
+    args.calEventId = window.parseInt(this.id);
+    args._eventId = utils.guid();
+
+    removeCallback = function (args) {
+        var result = JSON.parse(unescape(args.result)),
+            errorObj;
+
+        if (result._success) {
+            if (successCallback) {
+                successCallback();
+            }
+        } else {
+            if (errorCallback) {
+                errorObj = new CalendarError(result.code);
+                errorCallback(errorObj);
+            }
+        }
+    };
+
+    window.webworks.event.once(_ID, args._eventId, removeCallback);
+    return window.webworks.execAsync(_ID, "remove", args);
+};
+
+CalendarEvent.prototype.createExceptionEvent = function (originalStartTime) {
+    var properties = {},
+        key,
+        exceptionEvent;
+
+    for (key in this) {
+        if (this.hasOwnProperty(key) && key !== "recurrence") {
+            properties[key] = this[key];
+        }
+    }
+
+    properties.id = null;
+    properties.parentId = this.id;
+    properties.originalStartTime = originalStartTime;
+
+    exceptionEvent = new CalendarEvent(properties);
+
+    if (!this.recurrence) {
+        this.recurrence = new CalendarRepeatRule();
+    }
+
+    if (!this.recurrence.exceptionDates) {
+        this.recurrence.exceptionDates = [];
+    }
+
+    this.recurrence.exceptionDates.push(originalStartTime instanceof Date ? originalStartTime : new Date(originalStartTime));
+
+    return exceptionEvent;
+};
+
+Object.defineProperty(CalendarEvent, "SENSITIVITY_NORMAL", {"value": 0});
+Object.defineProperty(CalendarEvent, "SENSITIVITY_PERSONAL", {"value": 1});
+Object.defineProperty(CalendarEvent, "SENSITIVITY_PRIVATE", {"value": 2});
+Object.defineProperty(CalendarEvent, "SENSITIVITY_CONFIDENTIAL", {"value": 3});
+
+Object.defineProperty(CalendarEvent, "TRANSPARENCY_FREE", {"value": 0});
+Object.defineProperty(CalendarEvent, "TRANSPARENCY_TENTATIVE", {"value": 1});
+Object.defineProperty(CalendarEvent, "TRANSPARENCY_BUSY", {"value": 2});
+Object.defineProperty(CalendarEvent, "TRANSPARENCY_OUT_OF_OFFICE", {"value": 3});
+
+module.exports = CalendarEvent;
---
+++ ext/pim.calendar/client.js
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2012 Research In Motion Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var _self = {},
+    _ID = require("./manifest.json").namespace,
+    utils = require("./../../lib/utils"),
+    calendarUtils = require("./calendarUtils"),
+    CalendarEvent = require("./CalendarEvent"),
+    CalendarError = require("./CalendarError"),
+    CalendarFindOptions = require("./CalendarFindOptions"),
+    CalendarFolder = require("./CalendarFolder"),
+    CalendarRepeatRule = require("./CalendarRepeatRule"),
+    CalendarEventFilter = require("./CalendarEventFilter"),
+    Attendee = require("./Attendee");
+
+function invokeCallback(callback, args) {
+    if (callback && typeof callback === "function") {
+        callback(args);
+    }
+}
+
+function validateFindArguments(findOptions, onFindSuccess, onFindError) {
+    var error = false;
+
+    if (!onFindSuccess || typeof onFindSuccess !== "function" || !findOptions || typeof findOptions.limit !== "number" ||
+        !findOptions.filter || !findOptions.filter.prefix || typeof findOptions.filter.prefix !== "string") {
+        error = true;
+    } else {
+        switch (findOptions.detail) {
+        case CalendarFindOptions.DETAIL_MONTHLY:
+        case CalendarFindOptions.DETAIL_WEEKLY:
+        case CalendarFindOptions.DETAIL_FULL:
+        case CalendarFindOptions.DETAIL_AGENDA:
+            break;
+        default:
+            error = true;
+        }
+
+        if (!error && findOptions.sort && findOptions.sort.forEach) {
+            findOptions.sort.forEach(function (s) {
+                switch (s.fieldName) {
+                case CalendarFindOptions.SORT_FIELD_GUID:
+                case CalendarFindOptions.SORT_FIELD_SUMMARY:
+                case CalendarFindOptions.SORT_FIELD_LOCATION:
+                case CalendarFindOptions.SORT_FIELD_START:
+                case CalendarFindOptions.SORT_FIELD_END:
+                    break;
+                default:
+                    error = true;
+                }
+
+                if (s.desc === undefined || typeof s.desc !== "boolean") {
+                    error = true;
+                }
+            });
+        }
+
+        error = error ? error : findOptions.filter.start && !calendarUtils.isDate(findOptions.filter.start);
+        error = error ? error : findOptions.filter.end && !calendarUtils.isDate(findOptions.filter.end);
+
+        if (!error && findOptions.filter.folders) {
+            findOptions.filter.folders.forEach(function (folder) {
+                if (!folder || !folder.id || isNaN(parseInt(folder.id, 10)) || !folder.accountId || isNaN(folder.accountId, 10)) {
+                    error = true;
+                }
+            });
+        }
+    }
+
+    if (error) {
+        invokeCallback(onFindError, new CalendarError(CalendarError.INVALID_ARGUMENT_ERROR));
+    }
+
+    return !error;
+}
+
+function getFolderKeyList(folders) {
+    var folderKeys = [];
+
+    if (folders && folders.forEach) {
+        folders.forEach(function (folder) {
+            folderKeys.push({
+                "id": parseInt(folder.id, 10),
+                "accountId": parseInt(folder.accountId, 10)
+            });
+        });
+    }
+
+    return folderKeys;
+}
+
+_self.createEvent = function (properties, folder) {
+    var args = {},
+        key;
+
+    for (key in properties) {
+        if (properties.hasOwnProperty(key)) {
+            args[key] = properties[key];
+        }
+    }
+
+    args["folder"] = folder;
+
+    args.id = null;
+
+    return new CalendarEvent(args);
+};
+
+_self.getCalendarFolders = function () {
+    var obj = window.webworks.execSync(_ID, "getCalendarFolders"),
+        folders = [];
+
+    obj.forEach(function (props) {
+        folders.push(new CalendarFolder(props));
+    });
+
+    return folders;
+};
+
+_self.getDefaultCalendarFolder = function () {
+    var obj = window.webworks.execSync(_ID, "getDefaultCalendarFolder");
+    return new CalendarFolder(obj);
+};
+
+_self.getTimezones = function () {
+    return window.webworks.execSync(_ID, "getTimezones");
+};
+
+_self.findEvents = function (findOptions, onFindSuccess, onFindError) {
+    var callback,
+        eventId;
+
+    if (!validateFindArguments(findOptions, onFindSuccess, onFindError)) {
+        return;
+    }
+
+    findOptions.filter.folders = getFolderKeyList(findOptions.filter.folders);
+
+    callback = function (args) {
+        console.log(unescape(args.result));
+        var result = JSON.parse(unescape(args.result)),
+            events = result.events,
+            realEvents = [];
+
+        if (result._success) {
+            if (events) {
+                events.forEach(function (event) {
+                    // contactUtils.populateContact(contact);
+                    event["folder"] = result.folders[event.accountId + "-" + event.folderId];
+                    realEvents.push(new CalendarEvent(event));
+                });
+            }
+            console.log(events);
+            onFindSuccess(realEvents);
+        } else {
+            invokeCallback(onFindError, new CalendarError(result.code));
+        }
+    };
+
+    eventId = utils.guid();
+
+    window.webworks.event.once(_ID, eventId, callback);
+
+    return window.webworks.execAsync(_ID, "find", {
+        "_eventId": eventId,
+        "options": findOptions
+    });
+};
+
+_self.CalendarEvent = CalendarEvent;
+_self.CalendarRepeatRule = CalendarRepeatRule;
+_self.CalendarError = CalendarError;
+_self.CalendarFindOptions = CalendarFindOptions;
+_self.CalendarFolder = CalendarFolder;
+_self.CalendarEventFilter = CalendarEventFilter;
+_self.Attendee = Attendee;
+
+module.exports = _self;
